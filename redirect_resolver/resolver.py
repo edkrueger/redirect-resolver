9172045e7a85e4f9d83e267c5aedeffc15bf9095
edkrueger <edkrueger@gmail.com>
2021-03-04 11:50:07 -0600

    Add a timeout.

--- redirect_resolver/resolver.py
+++ redirect_resolver/resolver.py
@@ -1,5 +1,8 @@
 """Resolves the HTTP(S) redirect."""
 
+# pylint: disable=too-many-locals
+
+import csv
 from urllib.parse import urlparse
 
 import requests
@@ -15,16 +18,43 @@ HEADERS = {
 }
 
 
-def resolve_redirect(url):
+def resolve_redirect(url, timeout=None):
     """Resolves a redirect."""
 
     try:
         with requests.get(
-            url, allow_redirects=False, stream=True, headers=HEADERS
+            url, allow_redirects=False, stream=True, headers=HEADERS, timeout=timeout
         ) as response:
             redirect_url = response.headers.get("Location")
             return (
                 urlparse(redirect_url).netloc if redirect_url else urlparse(url).netloc
             )
-    except requests.exceptions.ConnectionError:
+    except (requests.exceptions.ConnectionError, requests.exceptions.InvalidURL):
         return None
+
+
+def redirect_resolver(in_path, out_path, in_column, out_column, timeout=None):
+    """Redirect resolver."""
+    with open(in_path, "r") as in_file, open(out_path, "w+") as out_file:
+        dict_reader = csv.DictReader(in_file)
+        in_fieldnames = dict_reader.fieldnames
+        out_fieldnames = [*in_fieldnames, out_column]
+        dict_writer = csv.DictWriter(out_file, fieldnames=out_fieldnames)
+        dict_writer.writeheader()
+
+        for in_row in dict_reader:
+            in_url = in_row[in_column]
+            url = "http://" + in_url if in_url else None
+            resolved_url = resolve_redirect(url, timeout) if url else None
+            out_row = {**in_row, out_column: resolved_url}
+            dict_writer.writerow(out_row)
+
+
+if __name__ == "__main__":
+    redirect_resolver(
+        "data/company_domain_list_filtered_100.csv",
+        "redirected_company_domain_list.csv",
+        "Company Domain Name",
+        "Redirected URL",
+        timeout=6,
+    )
